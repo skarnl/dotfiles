69fc3c43973589d189c92e1b53efca93fcdf539d
Oskar van Velden <oskar@rakso.nl>
2018-03-08 11:11:21 +0100

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,9 +4,9 @@
 #umask 0002
 
 #ALIASSES
-alias l='ls -alH'
 alias p='pstorm .'
-alias switch='echo "#### SWITCH TO \e[33m${PWD##*/}\e[39m ####" && pstorm . && z d:u'
-alias s='switch'
+alias c='code -n .'
 
 alias ntc='name-that-color'
+
+alias art='php artisan'
