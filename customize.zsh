d63eaed934fe9232ed47de0c17dcd02b2cee8db0
Oskar van Velden <oskar@rakso.nl>
2018-08-27 21:03:02 +0200

    Add rerere and recent alias

---
+++ customize.zsh
@@ -0,0 +1,32 @@
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+#ZSH_THEME="robbyrussell"
+ZSH_THEME="skar"
+#ZSH_THEME="pygmalion"
+
+## ANDERE THEMES overwegens waardy
+:
+##
+## kphoen
+## juanghurtado
+## bira - zelfde als gnzh (of andersom)
+## tjkirch_mod
+## afowler
+## gnzh**
+## eastwood*
+## pygmalion?
+##
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git)
+
+# User configuration
+
+# a checkout of this: https://github.com/seebi/dircolors-solarized.git
+# eval `dircolors /home/oskar/Downloads/dircolors/dircolors.256dark`
+
